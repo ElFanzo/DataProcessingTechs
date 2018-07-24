20f84eca68c6829c403afa4e19d3236be0d08957
Fanzil <fan.hakimov2010@yandex.ru>
2018-07-24 19:53:34 +0300

    modified module, added assert

--- NumSystems.py
+++ NumSystems.py
@@ -1,42 +1,64 @@
 """""
-Module to convert one number to another with different numeral system
+Module to convert one number to another with different numeral system.
 
 """""
 
 
-def convert(number, baseInit, baseFinal):
-    number = str(number)
-    baseInit = int(baseInit)
-    baseFinal = int(baseFinal)
-    if baseInit < 2 or baseInit > 36 or baseFinal < 2 or baseFinal > 36:
-        raise ValueError('Base of the number must be at least 2 and not more than 36!')
-    if baseInit == 10:
-        return convertDecimalToOther(number, baseFinal)
-    if baseFinal == 10:
-        return convertOtherToDecimal(number, baseInit)
-    return convertDecimalToOther(convertOtherToDecimal(number, baseInit), baseFinal)
+class Convert:
+
+    @staticmethod
+    def convert(number, baseInit, baseFinal):
+        """
+        Convert number with baseInit numeral system to number with baseFinal numeral system.
+        :param number: Init number
+        :param baseInit: Base of init number
+        :param baseFinal: Base of final number
+        :return: Final number
+        """
+        baseInit = int(baseInit)
+        baseFinal = int(baseFinal)
+        if baseInit < 2 or baseInit > 36 or baseFinal < 2 or baseFinal > 36:
+            raise ValueError('Base of the number must be at least 2 and not more than 36!')
+        if baseInit == 10:
+            return str(Convert._convertDecimalToOther(number, baseFinal))
+        if baseFinal == 10:
+            return str(Convert._convertOtherToDecimal(number, baseInit))
+        return str(Convert._convertDecimalToOther(Convert._convertOtherToDecimal(number, baseInit), baseFinal))
+
+    @staticmethod
+    def _convertOtherToDecimal(number, base):
+        return int(str(number), base=base)
+
+    @staticmethod
+    def _convertDecimalToOther(number, base):
+        number = int(number)
+        if base == 2:
+            return bin(number)[2:].upper()
+        if base == 8:
+            return oct(number)[2:].upper()
+        if base == 16:
+            return hex(number)[2:].upper()
+        res = ''
+        while number != 0:
+            mod = number % base
+            if base > 10:
+                t = mod + 48
+                if mod > 9:
+                    t += 7
+                res += chr(t)
+            else:
+                res += str(mod)
+            number //= base
+        return res[::-1].upper()
 
 
-def convertOtherToDecimal(number, base):
-    return int(number, base=base)
-
-
-def convertDecimalToOther(number, base):
-    if base == 2:
-        return bin(number)[2:].upper()
-    if base == 8:
-        return oct(number)[2:].upper()
-    if base == 16:
-        return hex(number)[2:].upper()
-    res = ''
-    while number != 0:
-        mod = number % base
-        if base > 10:
-            t = mod + 48
-            if mod > 9:
-                t += 7
-            res += chr(t)
-        else:
-            res += str(mod)
-        number //= base
-    return res[::-1].upper()
+if __name__ == '__main__':
+    assert Convert.convert(10, 10, 2) == '1010'
+    assert Convert.convert(1000, 2, 10) == '8'
+    assert Convert.convert(1000, 2, 8) == '10'
+    assert Convert.convert(1010, 2, 16) == 'A'
+    assert Convert.convert('Python', 35, 36) == 'MKVTTW'
+    E = int(Convert.convert('E', 16, 10))
+    DA = int(Convert.convert('DA', 16, 10))
+    BEC = int(Convert.convert('BEC', 16, 10))
+    assert E * DA == BEC
