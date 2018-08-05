c70b75a03e52d06133f3ce71db12cf3b945c5995
Fanzil <fan.hakimov2010@yandex.ru>
2018-08-06 00:33:53 +0300

    added unduplicating script

---
+++ CSV_Unduplicate.py
@@ -0,0 +1,21 @@
+"""
+Unduplicate .csv files, existing in the current directory.
+"""
+
+import os
+
+
+if __name__ == '__main__':
+    files = []
+    names = []
+    for file in os.listdir('.'):
+        if file.endswith('.csv'):
+            files.append(set(open(file, 'r', encoding='utf-8').read().split(',')))
+            names.append(file)
+
+    for i in range(1, len(files)):
+        for j in range(i):
+            files[i] = files[i].difference(files[j])
+
+        with open(names[i], 'w', encoding='utf-8') as file:
+            file.write(','.join(row for row in files[i]))
