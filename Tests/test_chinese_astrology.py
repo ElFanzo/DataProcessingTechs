cfd5f8cab57329e03c83c34da94fcab4135af77f
Fanzil <elfanzo@users.noreply.github.com>
2020-07-09 19:25:25 +0500

    Edit tests

--- Tests/test_chinese_astrology.py
+++ Tests/test_chinese_astrology.py
@@ -3,8 +3,8 @@ from unittest import main, TestCase
 from chinese_astrology import what_year
 
 
-class TestYear(TestCase):
-    def test_what_year(self):
+class TestWhatYear(TestCase):
+    def test_correct_year(self):
         self.assertEqual(
             what_year(519), "519 is the yellow pig year. An element is Earth."
         )
@@ -21,11 +21,19 @@ class TestYear(TestCase):
             what_year(1997), "1997 is the red ox year. An element is Fire."
         )
 
+    def test_wrong_year(self):
         with self.assertRaises(ValueError):
             what_year(0)
 
         with self.assertRaises(ValueError):
             what_year(-1)
+
+    def test_not_integer_year(self):
+        with self.assertRaises(ValueError):
+            what_year("2020")
+
+        with self.assertRaises(ValueError):
+            what_year("")
 
     @staticmethod
     def run_test():
--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
-from Tests.test_chinese_astrology import TestYear
-from Tests.test_num_systems import TestNumSys
+from Tests.test_chinese_astrology import TestWhatYear
+from Tests.test_num_systems import TestConvert
 
 if __name__ == "__main__":
-    TestYear.run_test()
-    TestNumSys.run_test()
+    TestWhatYear.run_test()
+    TestConvert.run_test()
