ba1fe9086466fdc99529f0a5dcf5c76f75c82551
Fanzil <elfanzo@users.noreply.github.com>
2020-07-09 13:45:43 +0500

    Remove class

--- Tests/test_num_systems.py
+++ Tests/test_num_systems.py
@@ -1,36 +1,36 @@
 from unittest import main, TestCase
 
-from num_systems import Convert
+from num_systems import convert
 
 
 class TestNumSys(TestCase):
     def test_convert(self):
-        self.assertEqual(Convert.convert(10, 10, 2), "1010")
-        self.assertEqual(Convert.convert(1000, 2, 10), "8")
-        self.assertEqual(Convert.convert("16", "10", "2"), "10000")
-        self.assertEqual(Convert.convert(1000, 2, 8), "10")
-        self.assertEqual(Convert.convert(1010, 2, 16), "A")
-        self.assertEqual(Convert.convert("Python", 35, 36), "MKVTTW")
+        self.assertEqual(convert(10, 10, 2), "1010")
+        self.assertEqual(convert(1000, 2, 10), "8")
+        self.assertEqual(convert("16", "10", "2"), "10000")
+        self.assertEqual(convert(1000, 2, 8), "10")
+        self.assertEqual(convert(1010, 2, 16), "A")
+        self.assertEqual(convert("Python", 35, 36), "MKVTTW")
 
-        E = int(Convert.convert("E", 16, 10))
-        DA = int(Convert.convert("DA", 16, 10))
-        BEC = int(Convert.convert("BEC", 16, 10))
+        E = int(convert("E", 16, 10))
+        DA = int(convert("DA", 16, 10))
+        BEC = int(convert("BEC", 16, 10))
         self.assertEqual(E * DA, BEC)  # EDA == BEC :)
 
         with self.assertRaises(ValueError):
-            Convert.convert(123, 1, 10)
+            convert(123, 1, 10)
         with self.assertRaises(ValueError):
-            Convert.convert(123, 38, 10)
+            convert(123, 38, 10)
         with self.assertRaises(ValueError):
-            Convert.convert(123, 4, 1)
+            convert(123, 4, 1)
         with self.assertRaises(ValueError):
-            Convert.convert(123, 4, 38)
+            convert(123, 4, 38)
         with self.assertRaises(ValueError):
-            Convert.convert(123, 4, "1e2")
+            convert(123, 4, "1e2")
         with self.assertRaises(ValueError):
-            Convert.convert(123, "23d", 4)
+            convert(123, "23d", 4)
         with self.assertRaises(ValueError):
-            Convert.convert("abc", 4, 5)
+            convert("abc", 4, 5)
 
     @staticmethod
     def run_test():
--- num_systems.py
+++ num_systems.py
@@ -1,70 +1,69 @@
-class Convert:
-    """Class for converting numbers to various numeral systems."""
+"""Module for converting numbers to various numeral systems."""
 
-    @staticmethod
-    def convert(number, base_init, base_final):
-        """Translate a number to another numeral system.
 
-        :param number: the initial number
-        :param base_init: a base of the initial number
-        :param base_final: a base of the final number
-        :return: a translated number
-        """
-        base_init = int(base_init)
-        base_final = int(base_final)
+def convert(number, base_init, base_final):
+    """Translate a number to another numeral system.
 
-        if not (1 < base_init < 37) or not (1 < base_final < 37):
-            raise ValueError(
-                "Base of the number must be at least 2 and not more than 36!"
-            )
+    :param number: the initial number
+    :param base_init: a base of the initial number
+    :param base_final: a base of the final number
+    :return: a translated number
+    """
+    base_init = int(base_init)
+    base_final = int(base_final)
 
-        if base_init == 10:
-            return str(Convert._convert_dec_to_other(number, base_final))
-        if base_final == 10:
-            return str(Convert._convert_to_dec(number, base_init))
-        return str(
-            Convert._convert_dec_to_other(
-                Convert._convert_to_dec(number, base_init), base_final
-            )
+    if not (1 < base_init < 37) or not (1 < base_final < 37):
+        raise ValueError(
+            "Base of the number must be at least 2 and not more than 36!"
         )
 
-    @staticmethod
-    def __convert_to_dec(number, base):
-        """Translate a number to the decimal numeral system.
+    if base_init == 10:
+        return str(__convert_dec_to_other(number, base_final))
+    if base_final == 10:
+        return str(__convert_to_dec(number, base_init))
+    return str(
+        __convert_dec_to_other(
+            __convert_to_dec(number, base_init), base_final
+        )
+    )
 
-        :param number: the initial number
-        :param base: a base of the initial number
-        :return: a translated number
-        """
-        return int(str(number), base=base)
 
-    @staticmethod
-    def __convert_dec_to_other(number, base):
-        """Translate the decimal number to another numeral system.
+def __convert_to_dec(number, base):
+    """Translate a number to the decimal numeral system.
 
-        :param number: the initial number
-        :param base: a base of the final number
-        :return: a translated number
-        """
-        number = int(number)
+    :param number: the initial number
+    :param base: a base of the initial number
+    :return: a translated number
+    """
+    return int(str(number), base=base)
 
-        if base == 2:
-            return bin(number)[2:].upper()
-        if base == 8:
-            return oct(number)[2:].upper()
-        if base == 16:
-            return hex(number)[2:].upper()
 
-        res = ""
-        while number:
-            mod = number % base
-            if base > 10:
-                t = mod + 48
-                if mod > 9:
-                    t += 7
-                res += chr(t)
-            else:
-                res += str(mod)
-            number //= base
+def __convert_dec_to_other(number, base):
+    """Translate the decimal number to another numeral system.
 
-        return res[::-1].upper()
+    :param number: the initial number
+    :param base: a base of the final number
+    :return: a translated number
+    """
+    number = int(number)
+
+    if base == 2:
+        return bin(number)[2:].upper()
+    if base == 8:
+        return oct(number)[2:].upper()
+    if base == 16:
+        return hex(number)[2:].upper()
+
+    res = ""
+    while number:
+        mod = number % base
+        if base > 10:
+            t = mod + 48
+            if mod > 9:
+                t += 7
+            res += chr(t)
+        else:
+            res += str(mod)
+        number //= base
+
+    return res[::-1].upper()
